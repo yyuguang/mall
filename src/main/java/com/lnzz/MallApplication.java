92a1eecd6d7f795dc877c8d396ac965715a97b18
yyuguang <790335255@qq.com>
2020-01-16 17:24:37 +0800

    项目框架搭建

---
+++ src/main/java/com/lnzz/MallApplication.java
@@ -0,0 +1,21 @@
+package com.lnzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName：MallApplication
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/1/16 17:18
+ * @Description:
+ */
+@SpringBootApplication
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}
